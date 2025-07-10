391172c25450ba2f008c6eb556816434ef4781ce
Nasser Ayoub <ayoub.nasser2018@gmail.com>
2025-07-10 01:49:41 +0100

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
-import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 const Footer = () => {
     return (
@@ -12,9 +12,9 @@ const Footer = () => {
                         <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
                     </Link>
 
-                    <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+                    <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
                         {FOOTER_LINKS.map((columns) => (
-                            <FooterColumn title={columns.title}>
+                            <FooterColumn key={columns.title} title={columns.title}>
                                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
                                     {columns.links.map((link) => (
                                         <Link href="/" key={link}>
@@ -33,9 +33,7 @@ const Footer = () => {
                                         key={link.label}
                                         className="flex gap-4 md:flex-col lg:flex-row"
                                     >
-                                        <p className="whitespace-nowrap">
-                                            {link.label}:
-                                        </p>
+                                        <p className="whitespace-nowrap">{link.label}:</p>
                                         <p className="medium-14 whitespace-nowrap text-blue-70">
                                             {link.value}
                                         </p>
@@ -59,16 +57,18 @@ const Footer = () => {
                 </div>
 
                 <div className="border bg-gray-20" />
-                <p className="regular-14 w-full text-center text-gray-30">2023 Hilink | All rights reserved</p>
+                <p className="regular-14 w-full text-center text-gray-30">
+                    2023 Hilink | All rights reserved
+                </p>
             </div>
         </footer>
-    )
-}
+    );
+};
 
 type FooterColumnProps = {
     title: string;
     children: React.ReactNode;
-}
+};
 
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
     return (
@@ -76,7 +76,7 @@ const FooterColumn = ({ title, children }: FooterColumnProps) => {
             <h4 className="bold-18 whitespace-nowrap">{title}</h4>
             {children}
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
